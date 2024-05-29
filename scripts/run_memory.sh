0ff4a83f74d04246638c122b993ae95a68b9fb62
silviase <silviase@liat300.com>
2024-05-29 22:12:34 +0900

    05/29 End

--- scripts/run_memory.sh
+++ scripts/run_memory.sh
@@ -3,6 +3,7 @@
 # PROMPT_ID=$3
 # RESULT_DIR=$4
 # DEVICES=$5
+
 # 5/28 
 ### extract -- thumb -- llava
 bash scripts/run_single_inference.sh thumb liuhaotian/llava-v1.5-7b visual_context inference 5
@@ -28,11 +29,27 @@ bash scripts/run_single_inference.sh flickr8k-expert Yi-VL-6B caption inference 
 ### extract -- composite -- yi-6b
 bash scripts/run_single_inference.sh composite Yi-VL-6B visual_context inference 6
 bash scripts/run_single_inference.sh composite Yi-VL-6B caption inference 6
+
+# 5/29
+### extract -- thumb -- llava -- (visce / caps / refs)
+bash scripts/run_single_eval_simple.sh thumb liuhaotian/llava-v1.5-7b visce 4 --debug --use_cand --use_context 
+bash scripts/run_single_eval_simple.sh thumb liuhaotian/llava-v1.5-13b visce 4 --debug --use_cand --use_context 
+bash scripts/run_single_eval_simple.sh thumb liuhaotian/llava-v1.5-7b captions 5 --debug --use_cand --use_caption
+bash scripts/run_single_eval_simple.sh thumb liuhaotian/llava-v1.5-13b captions 5 --debug --use_cand --use_caption 
+bash scripts/run_single_eval_simple.sh thumb liuhaotian/llava-v1.5-7b references 6 --debug --use_cand --use_refs 
+bash scripts/run_single_eval_simple.sh thumb liuhaotian/llava-v1.5-13b references 6 --debug --use_cand --use_refs 
+### extract -- flickr8k-expert -- llava -- (visce / caps / refs)
+bash scripts/run_single_eval_simple.sh flickr8k-expert liuhaotian/llava-v1.5-7b visce 4 --debug --use_cand --use_context 
+bash scripts/run_single_eval_simple.sh flickr8k-expert liuhaotian/llava-v1.5-13b visce 5 --debug --use_cand --use_context 
+bash scripts/run_single_eval_simple.sh flickr8k-expert liuhaotian/llava-v1.5-7b captions 6 --debug --use_cand --use_caption
 # 未着手=================================================================================================
+bash scripts/run_single_eval_simple.sh flickr8k-expert liuhaotian/llava-v1.5-13b captions 4 --debug --use_cand --use_caption 
+bash scripts/run_single_eval_simple.sh flickr8k-expert liuhaotian/llava-v1.5-7b references 5 --debug --use_cand --use_refs 
+bash scripts/run_single_eval_simple.sh flickr8k-expert liuhaotian/llava-v1.5-13b references 6 --debug --use_cand --use_refs 
 
 ### extract -- thumb -- yi-34b
-bash scripts/run_single_inference.sh thumb Yi-VL-34B visual_context inference 5
-bash scripts/run_single_inference.sh thumb Yi-VL-34B visual_context inference 6
+# bash scripts/run_single_inference.sh thumb Yi-VL-34B visual_context inference 5
+# bash scripts/run_single_inference.sh thumb Yi-VL-34B visual_context inference 6
 
 ### extract -- f8kcf -- llava
 ### extract -- pascal50s -- llava
